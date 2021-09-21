48f70ceedd8928aad55d5eee886be20dabbd8bd2
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-09-21 22:29:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let age = 18;
+
+if (age == 18) {
+
+    document.write("toegang geaccepteerd, u bent 18 jaar");
+
+} else if (age > 18) {
+
+    document.write("toegang geaccepteerd, u bent ouder dan 18 jaar");
+} else {
+
+    document.write("u bent jonger dan 18 jaar, u wordt vriendelijk verzocht buiten te blijven");
+}
+
+
+
+let isfemale = true;
+
+if (isfemale) {
+    document.write("welkom bij ladiesnight");
+
+} else {
+    document.write("Helaas is het vandaag ladiesnight, kom de volgende keer terug")
+}
+
+
+
+let bob = true;
+
+if (bob) {
+
+    document.write("u mag autorijden");
+
+} else {
+    document.write("u mag niet rijden");
+}
